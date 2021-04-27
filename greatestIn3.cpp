27098158ae69e9e4d713fcb5c81add2c2c1c0beb
Ayush Rameja <ayushrwork@gmail.com>
2021-04-27 09:11:37 +0530

    Greatest number among 3 number

---
+++ greatestIn3.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+int main(){
+     int a,b,c;
+     cout<<"Enter the first number: ";
+     cin>>a;
+     cout<<"Enter the Second number: ";
+     cin>>b;
+     cout<<"Enter the third number: ";
+     cin>>c;
+     if(a>b && a>c){
+          cout<<a<<" is the greatest number.";
+     }
+     else if(b>a && b>c){
+          cout<<b<<" is the greatest number.";
+     }
+     else{
+          cout<<c<<" is the greatest number.";
+     }
+
+     return 0;
+}
